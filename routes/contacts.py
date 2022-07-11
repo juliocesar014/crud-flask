f8c316f3a120e7e21438b3575152594717c8d716
Júlio César Agripino de Oliveira <juliocesar014@live.com>
2022-07-11 20:15:35 -0300

    criação CRUD usando flask/python

---
+++ routes/contacts.py
@@ -0,0 +1,43 @@
+from flask import Blueprint, render_template, request, redirect
+from models.contact import Contact
+from utils.db import db
+
+
+contacts = Blueprint("contacts", __name__)
+
+
+@contacts.route("/")
+def home():
+    contacts = Contact.query.all()
+    return render_template("index.html", contacts=contacts)
+
+
+@contacts.route("/new", methods=["POST"])
+def add_contact():
+    fullname = request.form["fullname"]
+    email = request.form["email"]
+    phone = request.form["phone"]
+
+    new_contact = Contact(fullname, email, phone)
+
+    db.session.add(new_contact)
+    db.session.commit()
+
+    return redirect("/")
+
+
+@contacts.route("/update")
+def update():
+    return "update contact"
+
+
+@contacts.route("/delete/<id>")
+def delete(id):
+    contact = Contact.query.get(id)
+    print(contact)
+    return "delete contact"
+
+
+@contacts.route("/about")
+def about():
+    return render_template("about.html")
